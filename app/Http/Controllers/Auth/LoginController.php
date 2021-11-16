43a2b69140fb08b0ed58909d490ccff46d61ebca
William Oliveira <william.christopher.96@gmail.com>
2021-11-16 20:45:30 -0300

    removi umas linhas de teste

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -22,16 +22,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Get the login username to be used by the controller.
-     *
-     * @return string
-     */
-    public function username()
-    {
-        return 'username';
-    }
-
-    /**
      * Where to redirect users after login.
      *
      * @var string
