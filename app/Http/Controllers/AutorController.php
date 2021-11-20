ae2d9f52624e3a87ed4bf9e64c3d83ab5f108b0e
William Oliveira <william.christopher.96@gmail.com>
2021-11-20 15:25:31 -0300

    added repository

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -12,55 +12,15 @@ class AutorController extends Controller
         return view('autor.cadastrar');
     }
 
-    public function store(Request $request)
-    {
-        Autor::create([
-            'nome' => $request->nome,
-            'email' => $request->email,
-            'endereco' => $request->endereco,
-            'telefone' => $request->telefone,
-        ]);
-
-        return "Autor cadastrado com sucesso!";
-    }
-
-    public function show($id)
-    {
-        $autor = Autor::findOrFail($id);
-        return view('autor.mostrar', ['autor' => $autor]);
-    }
-
     public function edit($id)
     {
         $autor = Autor::findOrFail($id);
         return view('autor.editar', ['autor' => $autor]);
     }
 
-    public function update(Request $request, $id)
-    {
-        $autor = Autor::findOrFail($id);
-
-        $autor->update([
-            'nome' => $request->nome,
-            'email' => $request->email,
-            'endereco' => $request->endereco,
-            'telefone' => $request->telefone,
-        ]);
-
-        return "Autor editado com sucesso!";
-    }
-
     public function delete($id)
     {
         $autor = Autor::findOrFail($id);
         return view('autor.deletar', ['autor' => $autor]);
-    }
-
-    public function destroy($id)
-    {
-        $autor = Autor::findOrFail($id);
-        $autor->delete();
-
-        return "Autor excluído com sucesso!";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +23,14 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // CRUD Autor
+
+//controller
 Route::get('/autor/cadastro', [App\Http\Controllers\AutorController::class, 'create']);
-Route::post('/autor/cadastro', [App\Http\Controllers\AutorController::class, 'store'])->name('cadastro_autor');
-Route::get('/autor/{id}', [App\Http\Controllers\AutorController::class, 'show']);
-Route::get('/autor/editar/{id}', [App\Http\Controllers\AutorController::class, 'edit']);
-Route::post('/autor/editar/{id}', [App\Http\Controllers\AutorController::class, 'update'])->name('edicao_autor');
 Route::get('/autor/deletar/{id}', [\App\Http\Controllers\AutorController::class, 'delete']);
-Route::post('/autor/deletar/{id}', [\App\Http\Controllers\AutorController::class, 'destroy'])->name('exclusao_autor');
+Route::get('/autor/editar/{id}', [App\Http\Controllers\AutorController::class, 'edit']);
+
+//repository
+Route::post('/autor/editar/{id}', [App\Repository\AutorRepository::class, 'update'])->name('edicao_autor');
+Route::get('/autor/{id}', [App\Repository\AutorRepository::class, 'show']);
+Route::post('/autor/cadastro', [App\Repository\AutorRepository::class, 'store'])->name('cadastro_autor');
+Route::post('/autor/deletar/{id}', [App\Repository\AutorRepository::class, 'destroy'])->name('exclusao_autor');
