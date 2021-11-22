3b81794294197cb0dfc9ab196ecde720b429ba27
Rafael Azevedo Lezama <rafa11al@hotmail.com>
2021-11-22 20:33:12 -0300

    fix no logout acho que tava importando duas vezes o bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,12 +22,20 @@
      
     <!-- Fontawesome para icones -->
     <script src="https://use.fontawesome.com/1ce878220b.js"></script>
-    
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+
             <div class="container">
+               <a class="navbar-brand ms-5" href="{{ url('/') }}">
+                    <div class="text-start" style="color: black;" id="logo">
+                        <i class="fa fa-book fa-1x" aria-hidden="true"></i>
+                        <i class="fa fa-flask fa-1x" style="color: #6351ce;" aria-hidden="true"></i>
+                    </div>
+                    
+                </a>
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Revista') }}
                 </a>
@@ -57,12 +65,13 @@
                                 </li>
                             @endif
                         @else
+
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" data-bs-target="logout" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" id="logout">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -84,9 +93,8 @@
             @yield('content')
         </main>
     </div>
-
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>   
 
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
             </a>
         </div>
         @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <div class="hidden fixed top-0 right-0 px-6 py-3 sm:block">
             @auth
             <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</i></a>
             @else
